1644109bc3f2e2c6ea12065ffc3f76fe5e930851
Saransh <saranshlingwal923@gmail.com>
2023-01-07 21:02:22 +0530

    updating version

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 // this is the backend file
 const bodyParser = require("body-parser");
 const express = require("express");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
+mongoose.set('strictQuery',false );
+mongoose.connect("mongodb://localhost:27017/todolistDB" , {useNewUrlParser: true});
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
 
-let workItems = [];
+// ITEMS SCHEMA 
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+//MONGOOSE MODEL FOR ITEMS SCHEMA
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item({
+    name:"Welcome to your todolist"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name:"<------ Hit this to delete an item."
+});
+
+const defaultItems = [item1 , item2 , item3];
+
+Item.insertMany(defaultItems , function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("successfully inserted into database.");
+    }
+});
+
+
+
+// let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+// let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,8 +50,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    let day = date.getDate();
-    res.render("list", { listTitle: day, newListItems: items });
+    res.render("list", { listTitle: "Today", newListItems: items });
 
 });
 
